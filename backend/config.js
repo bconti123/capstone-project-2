d8496421fd22de58d59d03c4243ef0dd1a7a6d61
bconti123 <bryantcconti@gmail.com>
2023-12-18 07:44:13 -0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -10,7 +10,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
+const getDatabaseUri = () => {
   return (process.env.NODE_ENV === "test")
       ? "streaming_service_test"
       : process.env.DATABASE_URL || "streaming_service";
