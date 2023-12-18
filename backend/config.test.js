9048063282fd2ab79d809e9a41d740b64ef7a54e
bconti123 <bryantcconti@gmail.com>
2023-12-18 07:54:41 -0800

    Update config test

--- backend/config.test.js
+++ backend/config.test.js
@@ -1,22 +1,25 @@
 "use strict";
 
 describe("config can come from env", function () {
-  test("works", function() {
+  test("works", () => {
     process.env.SECRET_KEY = "abc";
     process.env.PORT = "5000";
     process.env.DATABASE_URL = "other";
     process.env.NODE_ENV = "other";
+    process.env.API_KEY = "xyz";
 
     const config = require("./config");
     expect(config.SECRET_KEY).toEqual("abc");
     expect(config.PORT).toEqual(5000);
     expect(config.getDatabaseUri()).toEqual("other");
     expect(config.BCRYPT_WORK_FACTOR).toEqual(12);
+    expect(config.API_KEY).toEqual("xyz");
 
     delete process.env.SECRET_KEY;
     delete process.env.PORT;
     delete process.env.BCRYPT_WORK_FACTOR;
     delete process.env.DATABASE_URL;
+    delete process.env.API_KEY;
 
     expect(config.getDatabaseUri()).toEqual("streaming_service");
     process.env.NODE_ENV = "test";
