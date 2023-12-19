e5492f80c06a91adaa959bf8481fd02f16ca8a34
bconti123 <bryantcconti@gmail.com>
2023-12-18 23:37:21 -0800

    Still working on user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -78,7 +78,7 @@ class User {
 
   static async findAll() {
     const result = db.query(
-       `SELECT username,
+      `SELECT username,
         first_name AS "firstName",
         last_name AS "lastName",
         Email AS "email"
@@ -87,5 +87,87 @@ class User {
         ORDER BY username`
     );
     return result.rows;
+  }
+
+  static async get(username) {
+    const userResult = await db.query(
+      `SELECT username,
+              first_name AS "firstName",
+              last_name AS "lastName",
+              email,
+              is_admin AS "isAdmin"
+       FROM users
+       WHERE username = $1`,
+      [username]
+    );
+    const user = userResult.rows[0];
+
+    if (!user) throw new NotFoundError(`no user found: ${username}`);
+
+    const userMovieList = await db.query(
+      `SELECT m.favorite_id
+       FROM watch_list
+       WHERE m.user_id = $1`,
+      [user.id]
+    );
+
+    const userTVShowList = await db.query(
+      `SELECT tv.favorite_id
+       FROM watch_list
+       WHERE tv.user_id = $1`,
+      [user.id]
+    );
+
+    user.movie_list = userMovieList.map((m) => m.favorite_id);
+    user.tvshow_list = userTVShowList.map((tv) => tv.favorite_id);
+
+    return user;
+  }
+
+  static async update(username, data) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+    }
+
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      firstName: "first_name",
+      lastName: "last_name",
+      isAdmin: "is_admin",
+    });
+
+    const usernameVarIdx = "$" + (values.length + 1);
+
+    const querySql =
+      (`
+      UPDATE users
+      SET ${setCols}
+      WHERE username = $1
+      RETURNING username,
+                first_name AS "firstName",
+                last_name AS "lastName,
+                Email AS "email",
+                is_admin AS "isAdmin
+                `,
+      [usernameVarIdx]);
+
+    const result = await db.query(querySql, [...values, username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No user found: ${username}`);
+    return user;
+  }
+
+  static async remove(username) {
+    let result = await db.query(
+      `DELETE
+       FROM users
+       WHERE username = $1
+       RETURNING username`,
+      [username]
+    );
+
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`no user found: ${username}`);
   }
 }
