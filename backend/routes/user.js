be02635fa9b3a7d60e2dc68fc51deec12ca15af6
bconti123 <bryantcconti@gmail.com>
2023-12-26 10:35:43 -0800

    Created user route

---
+++ backend/routes/user.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Route for users
+
+const express = require("express");
+const { BadRequestError } = require("../expressError.js");
+
+// Models
+const User = require("../models/user.js");
+const { createToken } = require("../helper/token.js");
+
+// Schemas
+
+const router = express.Router();
+
+// POST /
+
+// GET /
+
+// GET /[username]
+
+// PATCH /[username]
+
+// DELETE /[username]
+
+// POST /[username]/movies/[movie_id]
+
+// POST /[username]/tvshows/[tvshow_id]
