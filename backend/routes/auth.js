5f8e8f4e462475db96284b23185585d1baf01485
bconti123 <bryantcconti@gmail.com>
2023-12-26 10:12:48 -0800

    Created Schemas

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,9 +9,18 @@ const router = new express.Router();
 const { createToken } = require("../helper/token.js");
 const { BadRequestError } = require("../expressError.js");
 
+const jsonschemas = require("jsonschema");
+const userAuthSchema = require("../schemas/userAuth.json")
+const userRegisterSchema = require("../schemas/userRegister.json")
+
 // POST /auth/token
 router.post("/token", async (req, res, next) => {
-    try { 
+    try {
+        const validator = jsonschemas.validate(req.body, userAuthSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
         const { username, password } = req.body;
         const user = await User.authenticate(username, password);
         const token = createToken(user);
@@ -23,9 +32,19 @@ router.post("/token", async (req, res, next) => {
 });
 // POST /auth/register
 router.post("/register", async (req, res, next) => {
-  const newUser = await User.register({ ...req.body, isAdmin: false });
-  const token = createToken({ token });
-  return res.status(201).json({ token });
+    try {
+        const validator = jsonschemas.validate( ...req.body, userRegisterSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
+        const newUser = await User.register({ ...req.body, isAdmin: false });
+        const token = createToken({ token });
+        return res.status(201).json({ token });
+    } catch (e) {
+        return next(e);
+    }
+
 });
 
 module.exports = router;
