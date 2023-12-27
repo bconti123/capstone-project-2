4e934544a87ffab86d4ef9ec0500bec5db38d031
bconti123 <bryantcconti@gmail.com>
2023-12-27 14:54:59 -0800

    Created movie and tv show routes

---
+++ backend/routes/movie.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Route for movies.
+const Movie = require("../models/movie.js");
+const express = require("express");
+const { BadRequestError } = require("../expressError.js");
+
+const router = express.Router();
+
+// GET /
+
+// GET /[movie_id]
+
+// POST /
+
+// PATCH /[movie_id]
+
+// DELETE /[movie_id]
---
+++ backend/routes/tvshow.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// Route for tv shows.
+const TV = require("../models/tvshow.js")
+const express = require("express");
+const { BadRequestError } = require("../expressError.js");
+
+const router = express.Router();
+
+// GET /
+
+// GET /[tvshow_id]
+
+// POST /
+
+// PATCH /[tvshow_id]
+
+// DELETE /[tvshow_id]
