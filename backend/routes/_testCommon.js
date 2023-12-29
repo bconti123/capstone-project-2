cc19cab72ee0cc154ad419c60998dca2dace81d8
bconti123 <bryantcconti@gmail.com>
2023-12-28 18:03:07 -0800

    Add testMovideids and testTVids

--- backend/routes/_testCommon.js
+++ backend/routes/_testCommon.js
@@ -10,6 +10,9 @@ const Movie = require("../models/movie.js");
 // TV Series
 const TV = require("../models/tvshow.js");
 
+const testMovieids = [];
+const testTVids = [];
+
 // Create makeup users, movies, and tv series in commonBeforeAll function.
 const commonBeforeAll = async () => {
   await db.query(`DELETE FROM users RETURNING username`);
@@ -40,6 +43,66 @@ const commonBeforeAll = async () => {
     password: "password3",
     isAdmin: false,
   });
+
+  // DO THIS LATER
+  testMovieids[0] = (
+    await Movie.add({
+      id: 1,
+      title: "movie1",
+      overview: "overview1",
+      release_date: "Jan 01, 2020",
+      poster_path: "/img.png",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
+  testMovieids[1] = (
+    await Movie.add({
+      id: 2,
+      title: "movie2",
+      overview: "overview2",
+      release_date: "Jan 01, 2020",
+      poster_path: "/img.png",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
+  testMovieids[2] = (
+    await Movie.add({
+      id: 3,
+      title: "movie3",
+      overview: "overview3",
+      release_date: "Jan 01, 2020",
+      poster_path: "/img.png",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
+
+  testTVids[0] = (
+    await TV.add({
+      id: 1,
+      name: "tv1",
+      overview: "overview1",
+      first_air_date: "Jan 01, 2020",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
+  testTVids[1] = (
+    await TV.add({
+      id: 2,
+      name: "tv2",
+      overview: "overview2",
+      first_air_date: "Jan 01, 2020",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
+  testTVids[2] = (
+    await TV.add({
+      id: 3,
+      name: "tv3",
+      overview: "overview3",
+      first_air_date: "Jan 01, 2020",
+      genres_id: [1, 2, 3],
+    })
+  ).id;
 };
 
 const commonBeforeEach = async () => {
@@ -64,4 +127,6 @@ module.exports = {
   commonAfterAll,
   u1Token,
   u2Token,
+  testMovieids,
+  testTVids,
 };
