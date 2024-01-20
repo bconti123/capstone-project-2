b5d53f748d78b60517966198017e45c40b39529e
bconti123 <bryantcconti@gmail.com>
2024-01-20 11:40:23 -0800

    Update Home.js

--- frontend/src/homepage/Home.js
+++ frontend/src/homepage/Home.js
@@ -1,19 +1,43 @@
-import React from "react";
-import { Button, Header, Segment } from "semantic-ui-react";
+import React, { useContext } from "react";
+import { Button, Header, Segment, Grid, Message, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import Broswe from "../browse/Browse";
+import UserContext from "../auth/UserContext";
 
+// Create Dashboard
+// currentUser -> Dashboard
+// otherwise -> Home
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
   return (
-    <Segment textAlign="center" vertical>
-      <Header as="h1">The Streaming Application</Header>
-
-      <Button primary as={Link} to="/login">
-        Login
-      </Button>
-      <Button primary as={Link} to="/signup">
-        Sign Up
-      </Button>
-    </Segment>
+    <>
+      {currentUser ? (
+        <>
+          <Broswe />
+        </>
+      ) : (
+        <>
+          <Grid textAlign="center" style={{ height: "75vh" }} verticalAlign="middle">
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as="h2" textAlign="center">
+                Watch anywhere! Unlimited movies, TV shows, MORE!
+              </Header>
+              <Message>
+                <Segment textAlign="center" vertical>
+                  <Button primary as={Link} to="/login">
+                    Login
+                  </Button>
+                  <Icon name="arrows alternate horizontal" />
+                  <Button primary as={Link} to="/signup">
+                    Sign Up
+                  </Button>
+                </Segment>
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </>
+      )}
+    </>
   );
 };
 
