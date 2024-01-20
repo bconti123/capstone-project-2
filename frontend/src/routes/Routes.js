1aad306aee9c74fd9761a04b5be2e81f40da33e8
bconti123 <bryantcconti@gmail.com>
2024-01-19 16:00:37 -0800

    Remove Container

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -10,17 +10,18 @@ import MediaList from "../media/MediaList";
 
 const RouterApp = ({ login, signup }) => {
   return (
-    <Container fluid>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route exact path="/login" element={<LoginForm login={login} />} />
-        <Route exact path="/signup" element={<SignupForm signup={signup} />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route exact path="/login" element={<LoginForm login={login} />} />
+      <Route exact path="/signup" element={<SignupForm signup={signup} />} />
 
-        <Route path="/movies" element={<MediaList mediaType="movies" filterType="popular" />} />
-        {/* Add more later */}
-        <Route path="*" element={<h1>404! ERROR</h1>} />
-      </Routes>
-    </Container>
+      <Route
+        path="/movies"
+        element={<MediaList mediaType="movies" filterType="popular" />}
+      />
+      {/* Add more later */}
+      <Route path="*" element={<h1>404! ERROR</h1>} />
+    </Routes>
   );
 };
 
