7142118753500cd523597c1be180fee626a889df
bconti123 <bryantcconti@gmail.com>
2024-01-21 21:39:46 -0800

    Create MediaDetail component

--- frontend/src/media/MediaDetail.js
+++ frontend/src/media/MediaDetail.js
@@ -1,8 +1,35 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Modal, Item, Image, Container } from "semantic-ui-react";
 
-const MediaDetail = ({item}) => {
-    return (
-        <Header>Hello!</Header>
-    )
-}
+
+const MediaDetail = ({ closeModal, open, selectedItem }) => {
+  return (
+    <Modal onClose={closeModal} open={open} style={{ width: "70%" }}>
+      <Modal.Content style={{ padding: 0, overflow: "hidden" }}>
+        {selectedItem && (
+          <Item>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${selectedItem.backdrop_path}`}
+              floated="left"
+              fluid
+              style={{ width: "100%" }}
+            />
+            <Item.Content style={{ marginBottom: "20px" }}>
+              <Container text>
+                <Header as="h1" textAlign="center">
+                  {selectedItem.title || selectedItem.name}
+                </Header>
+                <p>{selectedItem.overview}</p>
+                <p>vote average: {selectedItem.vote_average}</p>
+                <p>vote count: {selectedItem.vote_count}</p>
+                <p>{selectedItem.id}</p>
+              </Container>
+            </Item.Content>
+          </Item>
+        )}
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default MediaDetail;
