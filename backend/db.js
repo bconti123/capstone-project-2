785a9833feb2965ba8131f12a580c9655572cc54
bconti123 <bryantcconti@gmail.com>
2024-01-22 18:28:09 -0800

    Modify db.js for Render

--- backend/db.js
+++ backend/db.js
@@ -7,18 +7,18 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    host: "/var/run/postgresql",
-    database: getDatabaseUri(),
-    // connectionString: getDatabaseUri(),
+    // host: "/var/run/postgresql",
+    // database: getDatabaseUri(),
+    connectionString: getDatabaseUri(),
     ssl: {
       rejectUnauthorized: false,
     },
   });
 } else {
   db = new Client({
-    host: "/var/run/postgresql",
-    database: getDatabaseUri(),
-    // connectionString: getDatabaseUri(),
+    // host: "/var/run/postgresql",
+    // database: getDatabaseUri(),
+    connectionString: getDatabaseUri(),
   });
 }
 
