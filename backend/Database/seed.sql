5e600b8e609c5dabc4d153248fcebf9b73bde6a1
bconti123 <bryantcconti@gmail.com>
2024-01-24 22:38:14 -0800

    Modify CSS

--- backend/Database/seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (username, password, first_name, last_name, Email, is_admin) 
-VALUES
-('admin', 'admin', 'Admin', 'Admin', 'admin@localhost.com', TRUE),
-('user', 'user', 'User', 'User', 'user@localhost.com', FALSE);
-
