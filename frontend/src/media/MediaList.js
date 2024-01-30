d1bb8f2ecca7935ba3722bce69a19fa5b2f415ee
bconti123 <bryantcconti@gmail.com>
2024-01-29 20:17:47 -0800

    Implement MediaList for UserList

--- frontend/src/media/MediaList.js
+++ frontend/src/media/MediaList.js
@@ -7,14 +7,16 @@ import MediaItem from "./MediaItem";
 const MediaList = ({ mediaType, filterType }) => {
   const [media, setMedia] = useState(null);
 
-  const List = async (mediaType, filterType) => {
-    let media = await backendAPI.getMediaList(mediaType, filterType);
-    setMedia(media);
-    // console.log(media.results);
-  };
+
 
   useEffect(() => {
     // console.log("MediaList useEffect", mediaType, filterType);
+
+    const List = async (mediaType, filterType) => {
+      let media = await backendAPI.getMediaList(mediaType, filterType);
+      setMedia(media);
+      console.debug(media);
+    };
     List(mediaType, filterType);
   }, [mediaType, filterType]);
   if (!media) return <p>Loading...</p>;
