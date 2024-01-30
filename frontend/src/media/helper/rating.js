2fd25d85edda9e0e1cc3e5a8ad6ac08be9defdad
bconti123 <bryantcconti@gmail.com>
2024-01-29 21:52:51 -0800

    Change DefaultRating to rating

--- frontend/src/media/helper/rating.js
+++ frontend/src/media/helper/rating.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Rating } from "semantic-ui-react";
 
 const AverageRating = ({ vote_average }) => {
-  let avg_vote = parseFloat(vote_average).toFixed(2) / 2;
+  const [vote_avg, setVote_Avg] = useState(null);
+
+  useEffect(() => {
+    if (vote_average) {
+      let average = parseFloat((vote_average) / 2).toFixed(2);
+      setVote_Avg(average);
+    }
+  }, [vote_average])
+  
   return (
     <p>
-      <Rating icon="star" size="large" defaultRating={avg_vote} maxRating={5} disabled /> {avg_vote} 
+      <Rating icon="star" size="large" rating={vote_avg} maxRating={5} disabled /> {vote_avg} 
       /5
     </p>
   );
