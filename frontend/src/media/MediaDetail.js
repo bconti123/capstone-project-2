13d67b60b01133b0be5ae895570a37fe0936fc91
bconti123 <bryantcconti@gmail.com>
2024-01-29 21:52:00 -0800

    Import useEffect and useState

--- frontend/src/media/MediaDetail.js
+++ frontend/src/media/MediaDetail.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header, Modal, Item, Image, Container } from "semantic-ui-react";
 import AverageRating from "./helper/rating";
 import AddtoList from "./helper/AddtoList";
+import mediaAPI from "../helper/tmdb-api";
 
+const MediaDetail = ({ mediaType, closeModal, open, selectedItem }) => {
+  const [data, setData] = useState(null);
+  let type = mediaType === "movies" ? "movie" : "tv";
+  // useEffect(() => {
+  //   if (open) {
 
-const MediaDetail = ({ closeModal, open, selectedItem }) => {
+  //   } else {
+  //     console.debug("Not Open")
+  //   }
+  // }, [mediaType, selectedItem.id]);
+  useEffect(() => {
+    if (open) {
+      const GetDetail = async (mediaType, id) => {
+        let detail = await mediaAPI.MediaInfo(mediaType, id);
+        setData(detail.data);
+      };
+      GetDetail(type, selectedItem.id);
+    }
+  }, [type, selectedItem, open]);
+
   return (
     <Modal onClose={closeModal} open={open} style={{ width: "70%" }}>
       <Modal.Content style={{ padding: 0, overflow: "hidden" }}>
         {selectedItem && (
           <Item>
             <Image
-              src={`https://image.tmdb.org/t/p/w500/${selectedItem.backdrop_path}`}
+              src={`https://image.tmdb.org/t/p/w500/${data?.backdrop_path}`}
               floated="left"
               fluid
               style={{ width: "100%" }}
@@ -22,7 +41,7 @@ const MediaDetail = ({ closeModal, open, selectedItem }) => {
                   {selectedItem.title || selectedItem.name}
                 </Header>
                 <p>{selectedItem.overview}</p>
-                <AverageRating vote_average={selectedItem.vote_average}/>
+                <AverageRating vote_average={data?.vote_average} />
                 <p>{selectedItem.id}</p>
                 <AddtoList selectedItem={selectedItem} />
               </Container>
