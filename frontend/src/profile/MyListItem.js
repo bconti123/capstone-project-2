e3128ac2207cbe8284cdaedb6844bfea0ec3a6e4
bconti123 <bryantcconti@gmail.com>
2024-01-29 20:17:09 -0800

    Create MyListItem.js

---
+++ frontend/src/profile/MyListItem.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { GridColumn, Image} from "semantic-ui-react";
+import MediaDetail from "../media/MediaDetail";
+
+const MyListItem = ({ media }) => {
+  const [open, setOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const openModal = (media) => {
+    setSelectedItem(media);
+    setOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedItem(null);
+    setOpen(false);
+  };
+
+
+  return (
+    <>
+      <GridColumn key={media.id}>
+        <Image
+          src={`https://image.tmdb.org/t/p/w200/${media.poster_path}`}
+          bordered
+          onClick={(e) => {
+            e.preventDefault();
+            openModal(media);
+          }}
+          style={{
+            width: "200px",
+            height: "300px",
+            cursor: "pointer",
+          }}
+        />
+      </GridColumn>
+      <MediaDetail
+        closeModal={closeModal}
+        open={open}
+        selectedItem={selectedItem}
+      />
+    </>
+  );
+};
+
+export default MyListItem;
