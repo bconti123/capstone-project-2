5460b4e6079c200addb8cf108cd616a69ed92909
bconti123 <bryantcconti@gmail.com>
2024-01-30 20:25:26 -0800

    Add video helper

---
+++ frontend/src/media/helper/video.js
@@ -0,0 +1,24 @@
+import { Embed } from "semantic-ui-react";
+
+const VideoURL = ({ key, url }) => {
+  const thumbnailURL = `https://img.youtube.com/vi/${url}/maxresdefault.jpg`;
+  return (
+    <>
+      <Embed
+        key={key}
+        active
+        id={url}
+        placeholder={thumbnailURL}
+        source="youtube"
+        iframe={{
+          allowFullScreen: true,
+          style: {
+            padding: 10,
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default VideoURL;
