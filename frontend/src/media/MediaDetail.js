757e3780e836637586323baee44a9083c6f5d779
bconti123 <bryantcconti@gmail.com>
2024-01-30 20:24:50 -0800

    Add Video Data

--- frontend/src/media/MediaDetail.js
+++ frontend/src/media/MediaDetail.js
@@ -1,33 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { Header, Modal, Item, Image, Container } from "semantic-ui-react";
+import {
+  Header,
+  Modal,
+  Item,
+  Image,
+  Container,
+  Divider,
+} from "semantic-ui-react";
 import AverageRating from "./helper/rating";
 import AddtoList from "./helper/AddtoList";
 import mediaAPI from "../helper/tmdb-api";
+import VideoURL from "./helper/video";
 
-const MediaDetail = ({ mediaType, closeModal, open, selectedItem }) => {
+const MediaDetail = ({
+  mediaType,
+  closeModal,
+  open,
+  selectedItem,
+  setSelectedItem,
+}) => {
   const [data, setData] = useState(null);
+  const [videoData, setVideoData] = useState(null);
   let type = mediaType === "movies" ? "movie" : "tv";
-  // useEffect(() => {
-  //   if (open) {
 
-  //   } else {
-  //     console.debug("Not Open")
-  //   }
-  // }, [mediaType, selectedItem.id]);
   useEffect(() => {
     if (open) {
       const GetDetail = async (mediaType, id) => {
         let detail = await mediaAPI.MediaInfo(mediaType, id);
         setData(detail.data);
+        setVideoData(detail.data.videos.results);
+        console.debug("data: ", data);
+        console.debug("videos: ", videoData);
       };
       GetDetail(type, selectedItem.id);
     }
-  }, [type, selectedItem, open]);
+  }, [type, open, selectedItem]);
 
   return (
-    <Modal onClose={closeModal} open={open} style={{ width: "70%" }}>
+    <Modal
+      onClose={closeModal}
+      open={open}
+      style={{ width: "70%" }}
+      size="fullscreen"
+    >
       <Modal.Content style={{ padding: 0, overflow: "hidden" }}>
-        {selectedItem && (
+        {data && (
           <Item>
             <Image
               src={`https://image.tmdb.org/t/p/w500/${data?.backdrop_path}`}
@@ -38,12 +55,20 @@ const MediaDetail = ({ mediaType, closeModal, open, selectedItem }) => {
             <Item.Content style={{ marginBottom: "20px" }}>
               <Container text>
                 <Header as="h1" textAlign="center">
-                  {selectedItem.title || selectedItem.name}
+                  {data?.title || data?.name}
                 </Header>
-                <p>{selectedItem.overview}</p>
+                <p>{data?.overview}</p>
                 <AverageRating vote_average={data?.vote_average} />
-                <p>{selectedItem.id}</p>
                 <AddtoList selectedItem={selectedItem} />
+              </Container>
+            </Item.Content>
+            <Header as="h1" textAlign="center">
+              Video
+            </Header>
+            <Divider />
+            <Item.Content>
+              <Container>
+                <VideoURL key={videoData[0]?.key} url={videoData[0]?.key} />
               </Container>
             </Item.Content>
           </Item>
