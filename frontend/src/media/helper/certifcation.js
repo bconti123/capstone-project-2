099eaf0d85ff2df1168928ba0238f87e046a4726
bconti123 <bryantcconti@gmail.com>
2024-02-01 19:19:05 -0800

    Implement certfication for TV

--- frontend/src/media/helper/certifcation.js
+++ frontend/src/media/helper/certifcation.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header, Label, Segment } from "semantic-ui-react";
+import mediaAPI from "../../helper/tmdb-api";
 
 const CountryCertication = ({ data }) => {
-  const countryCode = data.release_dates.results;
-  const release_date = countryCode.find((item) => item.iso_3166_1 === "US");
-  console.debug(countryCode);
-  // Check if release_date is not undefined before accessing its properties
-  const certData = release_date
-    ? release_date.release_dates[0].certification
-    : "";
-  console.debug(certData);
+  let certData;
+  if (data.title) {
+    const countryCode = data.release_dates.results;
+    const release_date = countryCode.find((item) => item.iso_3166_1 === "US");
+    console.debug(countryCode);
+    // Check if release_date is not undefined before accessing its properties
+    certData = release_date
+      ? release_date.release_dates[0].certification
+      : "";
+    console.debug(certData);
+  }
+  const [content, setContent] = useState(null);
+  useEffect(() => {
+    if (data.name) {
+      const getContent = async () => {
+        try {
+          let TVdata = await mediaAPI.TVContent(data.id);
+          setContent(TVdata);
+          console.debug(content);
+        } catch (e) {
+          console.error("An error occurred:", e);
+        }
+      };
+      getContent();
+    }
+  }, [data]);
+  if (data.name) {
+    certData = content?.find((item) => item.iso_3166_1 === "US").rating
+  }
 
   return (
     <Label basic size="small" circular>
       <Header as="h3" textAlign="center" block>
-        Rated {certData}{" "}
+        {certData}
       </Header>
     </Label>
   );
