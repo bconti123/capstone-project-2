a8866e137b49f4e3054b61ffa088f2d1a1c16f54
bconti123 <bryantcconti@gmail.com>
2024-02-20 13:23:39 -0800

    Fixed Issues

--- frontend/src/media/helper/certifcation.js
+++ frontend/src/media/helper/certifcation.js
@@ -10,7 +10,7 @@ const CountryCertication = ({ data }) => {
     const release_date = countryCode.find((item) => item.iso_3166_1 === "US");
     console.debug(countryCode);
     // Check if release_date is not undefined before accessing its properties
-    certData = release_date ? release_date.release_dates[0].certification : "";
+    certData = release_date ? release_date.release_dates[0].certification : "N/A";
     console.debug(certData);
   }
   const [content, setContent] = useState(null);
@@ -33,7 +33,7 @@ const CountryCertication = ({ data }) => {
     console.debug(content);
   } else if (!data.name && !data.title) {
     certData = "N/A"
-  }
+  };
 
   return (
     <Label basic size="small" circular>
