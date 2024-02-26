f7cb02681577caebab3f1daf1b3f861ec01f898c
bconti123 <bryantcconti@gmail.com>
2024-02-26 13:16:16 -0800

    Move Episodes to season.js from MediaDetail

--- frontend/src/media/TV_helper/season.js
+++ frontend/src/media/TV_helper/season.js
@@ -1,19 +1,45 @@
-import React from "react";
-import { Dropdown } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Dropdown, Header } from "semantic-ui-react";
+import Episodes from "./episode";
 
 const Season = ({ data }) => {
+  const [selectedSeason, setSelectedSeason] = useState(data.seasons[0]);
   const options = data.seasons.map((season) => ({
     key: season.season_number,
     text: `Season ${season.season_number}`,
     value: season.season_number,
   }));
+
+  const handleChange = (e, { value }) => {
+    const selectedSeasonData = data.seasons.find(
+      (season) => season.season_number === value
+    );
+    setSelectedSeason(selectedSeasonData);
+    // console.debug("selected", selectedSeasonData);
+  };
   return (
-    <Dropdown
-      placeholder="Select Season"
-      selection
-      options={options}
-      value={data.seasons[0].season_number}
-    />
+    <>
+     <Header>
+      <Dropdown
+        placeholder="Select Season"
+        selection
+        options={options}
+        value={selectedSeason.season_number}
+        onChange={handleChange}
+      />
+      </Header>
+      {selectedSeason && (
+        <>
+          {/* Pass the selected season data to Episodes component */}
+          <Episodes
+            key={selectedSeason.id}
+            id={data.id}
+            season_number={selectedSeason.season_number}
+            episodes={selectedSeason.episodes}
+          />
+        </>
+      )}
+    </>
   );
 };
 
