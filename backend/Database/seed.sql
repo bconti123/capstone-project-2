655a73bfa6a5235f621364f1829b16c704ca0715
bconti123 <bryantcconti@gmail.com>
2024-09-17 19:52:35 -0700

    Modify seed.sql

--- backend/Database/seed.sql
+++ backend/Database/seed.sql
@@ -1,5 +1,6 @@
 INSERT INTO users (username, password, first_name, last_name, Email, is_admin) 
 VALUES
 ('admin', 'admin', 'Admin', 'Admin', 'admin@localhost.com', TRUE),
-('user', 'user', 'User', 'User', 'user@localhost.com', FALSE);
+('user', 'user', 'User', 'User', 'user@localhost.com', FALSE),
+('demo', 'password', 'Demo', 'User', 'demo@localhost.com', FALSE);
 
