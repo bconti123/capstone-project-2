e66d3730cbf94d61e6445e75e452cf2b2c725c8c
bconti123 <bryantcconti@gmail.com>
2024-09-18 20:03:07 -0700

    Add errors

--- frontend/src/auth/DemoLoginForm.js
+++ frontend/src/auth/DemoLoginForm.js
@@ -1,37 +1,43 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Segment, Form } from "semantic-ui-react";
+import { Button, Segment, Form, Message } from "semantic-ui-react";
 
-const DemoLoginForm = ({demo}) => {
-    const navigate = useNavigate();
+const DemoLoginForm = ({ demo }) => {
+  const navigate = useNavigate();
 
-    const [formErrors, setFormErrors] = useState([]);
-    const [load, setLoad] = useState(false);
+  const [formErrors, setFormErrors] = useState([]);
+  const [load, setLoad] = useState(false);
 
-    const handleSubmit = async (evt) => {
-        evt.preventDefault();
-        let result = await demo();
-        if (result.success) {
-            setLoad(false);
-            navigate("/");
-        } else {
-            setFormErrors(result.errors);
-            console.debug(formErrors);
-            setLoad(false);
-        }
-    };
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    setLoad(true);
+    let result = await demo();
+    console.debug("result: ", result);
+    if (result.success) {
+      setLoad(false);
+      navigate("/");
+    } else {
+      setFormErrors(result.errors);
+      console.debug("form errors: ", formErrors);
+      setLoad(false);
+    }
+  };
 
-    return (
-        <Segment>
-            <p>You don't have to create an account. You can demo login.</p>
-            <Form onSubmit={handleSubmit}>
-                <Button type="submit" primary loading={load}>
-                    {load ? "Loading..." : "Demo Login"}
-                </Button>
-            </Form>
-      </Segment>
-    );
-}
+  return (
+    <Segment>
+      <p>You don't have to create an account. You can demo login.</p>
+      <Form onSubmit={handleSubmit} size="large">
+        <Button type="submit" primary loading={load}>
+          {load ? "Loading..." : "Demo Login"}
+        </Button>
+      </Form>
+      {formErrors.length > 0 && <Message error list={formErrors} />}
+      {load && (
+        <Message>Please wait while the backend server starts up</Message>
+      )}
+    </Segment>
+  );
+};
 
 export default DemoLoginForm;
